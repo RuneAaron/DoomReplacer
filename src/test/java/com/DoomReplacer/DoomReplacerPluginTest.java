4914933ad591e2cccac363ccd2be9f9c6e4135da
Aaron Foster <222860616+RuneAaron@users.noreply.github.com>
2025-07-29 21:16:55 +0100

    Update test

--- src/test/java/com/DoomReplacer/DoomReplacerPluginTest.java
+++ src/test/java/com/DoomReplacer/DoomReplacerPluginTest.java
@@ -1,11 +1,9 @@
-package com.example;
-
-import com.DoomReplacer.DoomReplacerPlugin;
+package com.DoomReplacer;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest {
+public class DoomReplacerPluginTest {
 	public static void main(String[] args) throws Exception {
 		ExternalPluginManager.loadBuiltin(DoomReplacerPlugin.class);
 		RuneLite.main(args);
